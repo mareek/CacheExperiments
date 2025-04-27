1883ecc49b9bfa57924a6c3f3ad04b932b9e80f6
Matthieu Mourisson <mareek@gmail.com>
2025-04-27 22:53:33 +0200

    refactoring

--- CacheExperiments/BetterCache.cs
+++ CacheExperiments/BetterCache.cs
@@ -2,22 +2,25 @@
 
 public class BetterCache<TKey, TValue>(int capacity) : ISimpleCache<TKey, TValue>
 {
-    private readonly LinkedList _list = new(capacity);
+    private readonly ListItem[] _items = new ListItem[capacity];
+    private int _firstIndex = -1;
+    private int _lastIindex = -1;
+    private int _firstAvailbleIndex = 0;
 
     public TValue AddOrUpdate(TKey key, Func<TKey, TValue> addValueFactory, Func<TKey, TValue, TValue> updateValueFactory)
     {
         TValue newValue;
-        if (_list.TryGetKeyIndex(key, out int index))
+        if (TryGetKeyIndex(key, out int index))
         {
-            var oldValue = _list.GetValue(index);
+            var oldValue = GetValue(index);
             newValue = updateValueFactory(key, oldValue);
-            _list.SetValue(index, newValue);
-            _list.MoveToTop(index);
+            SetValue(index, newValue);
+            MoveToTop(index);
         }
         else
         {
             newValue = addValueFactory(key);
-            _list.AddOnTop(key, newValue);
+            AddOnTop(key, newValue);
         }
 
         return newValue;
@@ -26,100 +29,89 @@ public class BetterCache<TKey, TValue>(int capacity) : ISimpleCache<TKey, TValue
     public TValue GetOrAdd(TKey key, Func<TKey, TValue> factory)
     {
         TValue value;
-        if (_list.TryGetKeyIndex(key, out int index))
+        if (TryGetKeyIndex(key, out int index))
         {
-            value = _list.GetValue(index);
-            _list.MoveToTop(index);
+            value = GetValue(index);
+            MoveToTop(index);
         }
         else
         {
             value = factory(key);
-            _list.AddOnTop(key, value);
+            AddOnTop(key, value);
         }
 
         return value;
     }
 
-    private class LinkedList(int capacity)
+    private void AddOnTop(TKey key, TValue value)
     {
-        private readonly ListItem[] _items = Enumerable.Range(0, capacity)
-                                                       .Select(_ => new ListItem(-1, default!, default!, -1))
-                                                       .ToArray();
-        private int _firstIndex = -1;
-        private int _lastIindex = -1;
-        private int _firstAvailbleIndex = 0;
-
-        public void AddOnTop(TKey key, TValue value)
+        ListItem newItem = new(-1, key, value, _firstIndex);
+        if (_firstAvailbleIndex < _items.Length)
         {
-            ListItem newItem = new(-1, key, value, _firstIndex);
-            if (_firstAvailbleIndex < _items.Length)
-            {
-                if (_firstIndex == -1)
-                    _lastIindex = _firstAvailbleIndex;
-                else
-                    _items[_firstIndex] = _items[_firstIndex].WithPreviousIndex(_firstAvailbleIndex);
-
-                _items[_firstAvailbleIndex] = newItem;
-                _firstIndex = _firstAvailbleIndex;
-                _firstAvailbleIndex++;
-            }
+            if (_firstIndex == -1)
+                _lastIindex = _firstAvailbleIndex;
             else
-            {
-                var newLastIndex = _items[_lastIindex].PreviousIndex;
-                _items[_lastIindex] = newItem;
-                _items[_firstIndex] = _items[_firstIndex].WithPreviousIndex(_lastIindex);
-                _items[newLastIndex] = _items[newLastIndex].WithNextIndex(-1);
-                _firstIndex = _lastIindex;
-                _lastIindex = newLastIndex;
-            }
+                _items[_firstIndex] = _items[_firstIndex].WithPreviousIndex(_firstAvailbleIndex);
+
+            _items[_firstAvailbleIndex] = newItem;
+            _firstIndex = _firstAvailbleIndex;
+            _firstAvailbleIndex++;
         }
-
-        public TValue GetValue(int index) => _items[index].Value;
-
-        public void SetValue(int index, TValue newValue) => _items[index] = _items[index].WithValue(newValue);
-
-        public void MoveToTop(int index)
+        else
         {
-            if (index == _firstIndex)
-                return;
-
-            var item = _items[index];
-            if (item.PreviousIndex != -1)
-                _items[item.PreviousIndex] = _items[item.PreviousIndex].WithNextIndex(item.NextIndex);
-
-            if (item.NextIndex != -1)
-                _items[item.NextIndex] = _items[item.NextIndex].WithPreviousIndex(item.PreviousIndex);
-
-            _items[_firstIndex] = _items[_firstIndex].WithPreviousIndex(index);
-            _items[index] = item.WithNextIndex(_firstIndex).WithPreviousIndex(-1);
-            _firstIndex = index;
+            var newLastIndex = _items[_lastIindex].PreviousIndex;
+            _items[_lastIindex] = newItem;
+            _items[_firstIndex] = _items[_firstIndex].WithPreviousIndex(_lastIindex);
+            _items[newLastIndex] = _items[newLastIndex].WithNextIndex(-1);
+            _firstIndex = _lastIindex;
+            _lastIindex = newLastIndex;
         }
+    }
 
-        public bool TryGetKeyIndex(TKey key, out int index)
+    private TValue GetValue(int index) => _items[index].Value;
+
+    private void SetValue(int index, TValue newValue) => _items[index] = _items[index].WithValue(newValue);
+
+    private void MoveToTop(int index)
+    {
+        if (index == _firstIndex)
+            return;
+
+        var item = _items[index];
+
+        _items[item.PreviousIndex] = _items[item.PreviousIndex].WithNextIndex(item.NextIndex);
+        if (item.NextIndex != -1)
+            _items[item.NextIndex] = _items[item.NextIndex].WithPreviousIndex(item.PreviousIndex);
+
+        _items[_firstIndex] = _items[_firstIndex].WithPreviousIndex(index);
+        _items[index] = item.WithNextIndex(_firstIndex).WithPreviousIndex(-1);
+        _firstIndex = index;
+    }
+
+    private bool TryGetKeyIndex(TKey key, out int index)
+    {
+        index = _firstIndex;
+        while (index != -1)
         {
-            index = _firstIndex;
-            while (index != -1)
-            {
-                var currentItem = _items[index];
-                if (EqualityComparer<TKey>.Default.Equals(key, currentItem.Key))
-                    return true;
+            var currentItem = _items[index];
+            if (EqualityComparer<TKey>.Default.Equals(key, currentItem.Key))
+                return true;
 
-                index = currentItem.NextIndex;
-            }
-
-            return false;
+            index = currentItem.NextIndex;
         }
 
-        private struct ListItem(int previousIndex, TKey key, TValue value, int nextIndex)
-        {
-            public int PreviousIndex { get; } = previousIndex;
-            public TKey Key { get; } = key;
-            public TValue Value { get; } = value;
-            public int NextIndex { get; } = nextIndex;
+        return false;
+    }
 
-            public ListItem WithPreviousIndex(int index) => new(index, Key, Value, NextIndex);
-            public ListItem WithNextIndex(int index) => new(PreviousIndex, Key, Value, index);
-            public ListItem WithValue(TValue value) => new(PreviousIndex, Key, value, NextIndex);
-        }
+    private struct ListItem(int previousIndex, TKey key, TValue value, int nextIndex)
+    {
+        public int PreviousIndex { get; } = previousIndex;
+        public TKey Key { get; } = key;
+        public TValue Value { get; } = value;
+        public int NextIndex { get; } = nextIndex;
+
+        public ListItem WithPreviousIndex(int index) => new(index, Key, Value, NextIndex);
+        public ListItem WithNextIndex(int index) => new(PreviousIndex, Key, Value, index);
+        public ListItem WithValue(TValue value) => new(PreviousIndex, Key, value, NextIndex);
     }
 }
